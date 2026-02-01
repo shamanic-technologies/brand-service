c90c60aabbc929babe2a0e30c25ac51a385aa59e
Kevin Lourd <kevin.lourd@gmail.com>
2026-02-01 10:50:21 +0800

    Use BRAND_SERVICE_DATABASE_URL env var

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 import 'dotenv/config';
 
-const connectionString = process.env.COMPANY_SERVICE_DATABASE_URL || process.env.DATABASE_URL;
+const connectionString = process.env.BRAND_SERVICE_DATABASE_URL || process.env.DATABASE_URL;
 
 const pool = new Pool({ 
   connectionString,
